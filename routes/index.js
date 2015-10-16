d0f250dce7abde6029acc998c69d93cd9670e71c
blue8wolf <lanbingling8@163.com>
2015-10-16 20:54:49 +0800

    测试 git默认文本编辑器是否成功修改回vi

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,33 @@ router.get('/admin', function(req, res, next) {
 router.get('/adminedit', function(req, res, next) {
   res.render('adminedit', { title: 'Edit tool info' });
 });
+
+
+
+/* password handle part */
+var crypto = require('crypto');
+var len=128;
+var iterations=12000;
+var hashfunc=function(pwd,salt,fn){
+	if(3==arguments.length){
+	crypto.pbkdf2(pwd,salt,iterations,len,function(err,hash){
+		fn(err,hash.toString('base64'));
+	});
+	}else{
+	fn=salt;
+	crypto.randomBytes(len,function(err,salt){
+	if(err) return fu(err);
+	salt=salt.toString('base64');
+	crypto.pbkdf2(pwd,salt,iterations,len,function(err,hash){
+	if(err) return fn(err);
+	fn(null,salt,hash.toString('base64'));
+	});
+	});
+	}	};
+
+
+
+
+
+
 module.exports = router;
